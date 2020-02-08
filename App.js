c6cdd00ada323c44607310392de12d6ef0495b9e
Khalled Meneouali <khalledmeneouali@MacBook-Pro-de-Khalled.local>
2020-02-08 19:28:09 +0100

    Login page

--- App.js
+++ App.js
@@ -4,28 +4,30 @@ import Constants from "expo-constants";
 import axios from "axios";
 import Header from "./components/Header";
 import Rooms from "./components/Rooms";
+import Login from "./components/Login";
 
 export default function App() {
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    axios
-      .get("https://airbnb-api.now.sh/api/room?city=paris")
-      .then(res => {
-        setData(res.data);
-      })
-      .catch(erro => {
-        console.warn(erro);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("https://airbnb-api.now.sh/api/room?city=paris")
+  //     .then(res => {
+  //       setData(res.data);
+  //     })
+  //     .catch(erro => {
+  //       console.warn(erro);
+  //     });
+  // }, []);
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <ScrollView horizontal={false}>
-        <Rooms data={data} />
-      </ScrollView>
-    </View>
+    // <View style={styles.container}>
+    //   <Header />
+    //   <ScrollView horizontal={false}>
+    //     <Rooms data={data} />
+    //   </ScrollView>
+    // </View>
+    <Login />
   );
 }
 
